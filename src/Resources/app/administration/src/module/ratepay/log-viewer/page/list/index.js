9e6a51d6653ee6474587eda9f9cd7cfb773417cb
Amir Arends <amir.arends@gmail.com>
2020-05-11 11:47:04 +0200

    [TASK] Add data-table for request logs

--- src/Resources/app/administration/src/module/ratepay/log-viewer/page/list/index.js
+++ src/Resources/app/administration/src/module/ratepay/log-viewer/page/list/index.js
@@ -28,40 +28,46 @@ Component.register('ratepay.logViewer.list', {
             return [{
                 property: 'id',
                 dataIndex: 'id',
-                label: this.$t('ratepay.log_viewer.global.labels.id'),
+                label: this.$t('ratepay.api_log.global.labels.id'),
                 allowResize: true
                 //primary: true
             }, {
                 property: 'operation',
                 dataIndex: 'operation',
-                label: this.$t('ratepay.log_viewer.global.labels.operation'),
+                label: this.$t('ratepay.api_log.global.labels.operation'),
                 allowResize: true
             }, {
                 property: 'suboperation',
                 dataIndex: 'suboperation',
-                label: this.$t('ratepay.log_viewer.global.labels.suboperation'),
+                label: this.$t('ratepay.api_log.global.labels.suboperation'),
                 allowResize: true
             }, {
                 property: 'status',
                 dataIndex: 'status',
-                label: this.$t('ratepay.log_viewer.global.labels.status'),
+                label: this.$t('ratepay.api_log.global.labels.status'),
+                allowResize: true
+            }, {
+                property: 'created_at',
+                dataIndex: 'created_at',
+                label: this.$t('ratepay.api_log.global.labels.created_at'),
                 allowResize: true
             }, {
                 property: 'request',
                 dataIndex: 'request',
-                label: this.$t('ratepay.log_viewer.global.labels.request'),
-                allowResize: true
+                label: this.$t('ratepay.api_log.global.labels.request'),
+                allowResize: false,
             }, {
                 property: 'response',
                 dataIndex: 'response',
-                label: this.$t('ratepay.log_viewer.global.labels.response'),
-                allowResize: true
+                label: this.$t('ratepay.api_log.global.labels.response'),
+                allowResize: false,
+                width: 100
             }];
         }
     },
 
     created() {
-        this.repository = this.repositoryFactory.create('ratepay_log_viewer');
+        this.repository = this.repositoryFactory.create('ratepay_api_log');
 
         this.repository
             .search(new Criteria(), Shopware.Context.api)
