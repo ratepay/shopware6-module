c37e698ebe7a8915706f7062b130b550610b3a03
Amir Arends <amir.arends@gmail.com>
2020-05-27 21:53:13 +0200

    [FIX] Remove js-debug stuff

--- src/Resources/app/storefront/src/RatepayPayments/RatepayPayments.js
+++ src/Resources/app/storefront/src/RatepayPayments/RatepayPayments.js
@@ -1,4 +1,4 @@
 export default class RatepayPayments extends Plugin {
-    alert();
+
 }
 
--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,7 +1,1 @@
 import RatepayPayments from './RatepayPayments/RatepayPayments';
-
-window.PluginManager.register('KlarnaPayments', KlarnaPayments, '[data-is-klarna-payments]');
-
-if (module.hot) {
-    module.hot.accept();
-}
