8c834b03254124843fc858ab00b6f78423fc1bcd
Amir Arends <amir.arends@gmail.com>
2020-05-28 13:41:44 +0200

    [FIX] bugfix for dfp and changed events + js integration for bank account toggler

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,1 +1,6 @@
+// Import all necessary Storefront plugins and scss files
 import RatepayPayments from './RatepayPayments/RatepayPayments';
+
+// Register them via the existing PluginManager
+const PluginManager = window.PluginManager;
+PluginManager.register('RatepayPayments', RatepayPayments, '[data-ratepay-payments]');
