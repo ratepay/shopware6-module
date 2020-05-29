7a1bc181a683dbd4e0e91f3a31c8ecb81452b642
Amir Arends <amir.arends@gmail.com>
2020-05-29 20:39:15 +0200

    [TASK] Add validator constraint

--- src/Components/Checkout/OrderValidationEventListener.php
+++ src/Components/Checkout/OrderValidationEventListener.php
@@ -4,28 +4,33 @@ declare(strict_types=1);
 
 namespace Ratepay\RatepayPayments\Components\Checkout;
 
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
+use Shopware\Core\Framework\Validation\DataValidator;
+use Shopware\Core\Framework\Validation\DataValidationDefinition;
+use Shopware\Core\Framework\Validation\Exception\ConstraintViolationException;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\Framework\Validation\BuildValidationEvent;
 use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class OrderValidationEventListener implements EventSubscriberInterface
 {
+
+    /** @var Validator */
+    private $validator;
+
     /** @var RequestStack */
     private $requestStack;
 
-    /** @var CartService */
-    private $cartService;
-
     public function __construct(
-        RequestStack $requestStack,
-        CartService $cartService
+        DataValidator $validator,
+        RequestStack $requestStack
     ) {
+        $this->validator = $validator;
         $this->requestStack  = $requestStack;
-        $this->cartService   = $cartService;
     }
 
     public static function getSubscribedEvents(): array
@@ -43,34 +48,31 @@ class OrderValidationEventListener implements EventSubscriberInterface
             return;
         }
 
-        $ratepayData = $request->request->get('ratepay');
+        $ratepayData = $request->request->all();
 
-        foreach ($ratepayData as $customerData) {
-            // can be an array, ex. birthday
+        $definition = new DataValidationDefinition('ratepay.validate.checkout');
+
+        foreach ($ratepayData['ratepay'] as $key => $customerData) {
             if (!is_array($customerData)){
                 if ($customerData === ''){
-                    return;
-                }
-            }
-            else {
-                foreach ($customerData as $singleValue) {
-                    if (!is_array($singleValue)) {
-                        if ($singleValue === ''){
-                            return;
-                        }
-                    }
+                    $definition->add($key, new NotBlank());
                 }
             }
         }
 
-        $context = $this->getContextFromRequest($request);
+        $violations = $this->validator->getViolations($ratepayData, $definition);
 
-        $cart = $this->cartService->getCart($context->getToken(), $context);
+        if (!$violations->count()) {
+            return;
+        }
+
+        throw new ConstraintViolationException($violations, $ratepayData);
 
     }
-
     private function getContextFromRequest(Request $request): SalesChannelContext
     {
         return $request->attributes->get(PlatformRequest::ATTRIBUTE_SALES_CHANNEL_CONTEXT_OBJECT);
     }
+
+
 }
