9d61e563343743a3d07011efec6b2e8bb5481e77
Amir Arends <amir.arends@gmail.com>
2020-06-01 21:41:27 +0200

    [FIX] Add missing drop table statements (RATESWSX-56)

--- src/Migration/Migration1576758772ApiLogTable.php
+++ src/Migration/Migration1576758772ApiLogTable.php
@@ -35,6 +35,6 @@ class Migration1576758772ApiLogTable extends MigrationStep
 
     public function updateDestructive(Connection $connection): void
     {
-        // implement update destructive
+        $connection->executeQuery("DROP TABLE ratepay_api_log");
     }
 }
