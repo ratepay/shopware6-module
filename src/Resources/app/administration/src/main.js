c3b2762528ba677929bc1b4e34eb00c23ca21e4f
Frederik Rommel <frederik.rommel@interlutions.de>
2020-06-02 15:47:58 +0200

    RATEPLUG-85: add copyright

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,2 +1,9 @@
+/*
+ * Copyright (c) 2020 RatePAY GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import './module/ratepay/profile-config';
 import './module/ratepay/log-viewer';
