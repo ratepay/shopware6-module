3c01c3c68011dcbea6af6ea08a2ffa404f589d46
Frederik Rommel <frederik.rommel@interlutions.de>
2020-06-02 15:58:19 +0200

    RATEPLUG-85: add copyright (in dfp branch)

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 RatePAY GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 // Import all necessary Storefront plugins and scss files
 import RatepayCheckout from './RatepayCheckout/RatepayCheckout';
 
