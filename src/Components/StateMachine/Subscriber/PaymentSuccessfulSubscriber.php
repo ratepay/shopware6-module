2c0a4bd6f2872a9751e5a65949c9be082312c915
Moritz Müller <moritz.mueller@interlutions.de>
2020-08-06 14:55:15 +0200

    [TASK] Moves subscriber and removes some state options from config
    Relates to RATESWSX-87

--- src/Components/StateMachine/Subscriber/PaymentSuccessfulSubscriber.php
+++ src/Components/StateMachine/Subscriber/PaymentSuccessfulSubscriber.php
@@ -6,12 +6,13 @@
  * file that was distributed with this source code.
  */
 
-namespace Ratepay\RatepayPayments\Components\PaymentHandler\Subscriber;
+namespace Ratepay\RatepayPayments\Components\StateMachine\Subscriber;
 
 
 use Ratepay\RatepayPayments\Components\PaymentHandler\Event\PaymentSuccessfulEvent;
 use Ratepay\RatepayPayments\Components\PluginConfig\Service\ConfigService;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionDefinition;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStates;
 use Shopware\Core\System\StateMachine\StateMachineRegistry;
 use Shopware\Core\System\StateMachine\Transition;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -23,6 +24,7 @@ class PaymentSuccessfulSubscriber implements EventSubscriberInterface
      * @var StateMachineRegistry
      */
     private $stateMachineRegistry;
+
     /**
      * @var ConfigService
      */
@@ -34,19 +36,19 @@ class PaymentSuccessfulSubscriber implements EventSubscriberInterface
         $this->configService = $configService;
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
-            PaymentSuccessfulEvent::class => 'changeTransactionState'
+            PaymentSuccessfulEvent::class => 'changeTransactionState',
         ];
     }
 
 
-    public function changeTransactionState(PaymentSuccessfulEvent $event)
+    public function changeTransactionState(PaymentSuccessfulEvent $event): void
     {
         $paymentMethod = $event->getTransaction()->getOrderTransaction()->getPaymentMethod();
         $newState = $this->configService->getPaymentStatusForMethod($paymentMethod);
-        if ($newState) {
+        if ($newState && $newState !== OrderTransactionStates::STATE_OPEN) {
             $this->stateMachineRegistry->transition(
                 new Transition(
                     OrderTransactionDefinition::ENTITY_NAME,
