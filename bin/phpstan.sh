c4e33940faccfdac72bef16a2707ae59d9cb5ba0
Frederik Rommel <frederik.rommel@webidea24.de>
2021-05-20 15:50:23 +0200

    update scripts

--- bin/phpstan.sh
+++ bin/phpstan.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(cd `dirname $0` && pwd)
 PLUGIN_DIR=$(dirname "$BASEDIR")
-SHOPWARE_DIR=$(dirname "$PLUGIN_DIR")/../../
+SHOPWARE_DIR=$(dirname "$PLUGIN_DIR")/../project
 
 echo "testing php7.2"
 /usr/bin/php7.2 "$SHOPWARE_DIR"/vendor/bin/phpstan analyse -c "$PLUGIN_DIR"/phpstan.neon;
