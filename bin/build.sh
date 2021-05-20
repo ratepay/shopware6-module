aa8887904a1a051883a8a8e50ed05761b12679cc
Frederik Rommel <frederik.rommel@webidea24.de>
2021-05-20 15:49:39 +0200

    update scripts

--- bin/build.sh
+++ bin/build.sh
@@ -2,23 +2,26 @@
 
 BASEDIR=$(cd `dirname $0` && pwd)
 PLUGIN_DIR=$(dirname "$BASEDIR")
+PLUGIN_NAME=$(basename "$PLUGIN_DIR")
 BUILD_DIR=$(dirname "PLUGIN_DIR")/build/
 
 mkdir -p build/
-tar -C "$PLUGIN_DIR"/../ --exclude-from="$BASEDIR"/.build_exclude -czf "$BUILD_DIR"/dist.tar.gz RpayPayments
-rm -rf "$BUILD_DIR"/dist/RpayPayments
-mkdir -p "$BUILD_DIR"/dist/RpayPayments
+tar -C "$PLUGIN_DIR"/../ --exclude-from="$BASEDIR"/.build_exclude -czf "$BUILD_DIR"/dist.tar.gz "$PLUGIN_NAME"
+rm -rf "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+mkdir -p "$BUILD_DIR"/dist/"$PLUGIN_NAME"
 tar -xzf "$BUILD_DIR"/dist.tar.gz -C "$BUILD_DIR"/dist/
 
-composer remove shopware/core shopware/administration shopware/storefront --no-install --ignore-platform-reqs -d "$BUILD_DIR"/dist/RpayPayments
-composer remove --unused --ignore-platform-reqs -d "$BUILD_DIR"/dist/RpayPayments
-composer install --ignore-platform-reqs --no-dev -d "$BUILD_DIR"/dist/RpayPayments
+composer remove shopware/core shopware/administration shopware/storefront --no-install --ignore-platform-reqs -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+composer remove --unused --ignore-platform-reqs -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
+composer install --ignore-platform-reqs --no-dev -d "$BUILD_DIR"/dist/"$PLUGIN_NAME"
 
-rm "$BUILD_DIR"/dist/RpayPayments/composer.json
-rm "$BUILD_DIR"/dist/RpayPayments/composer.lock
-cp "$PLUGIN_DIR"/composer.json "$BUILD_DIR"/dist/RpayPayments/composer.json
+rm "$BUILD_DIR"/dist/"$PLUGIN_NAME"/composer.json
+rm "$BUILD_DIR"/dist/"$PLUGIN_NAME"/composer.lock
+cp "$PLUGIN_DIR"/composer.json "$BUILD_DIR"/dist/"$PLUGIN_NAME"/composer.json
 
-(cd "$BUILD_DIR"/dist/RpayPayments/src/Resources && yarn install --no-dev)
+(cd "$BUILD_DIR"/dist/"$PLUGIN_NAME"/src/Resources && yarn install --no-dev)
+
+rm -rf "$BUILD_DIR"/dist/"$PLUGIN_NAME"/vendor/ratepay/php-library/tests
 rm -rf "$BUILD_DIR"/dist.tar.gz
 
-(cd "$BUILD_DIR"/dist && zip -r RpayPayments.zip RpayPayments)
+(cd "$BUILD_DIR"/dist && zip -r "$PLUGIN_NAME".zip "$PLUGIN_NAME")
--- bin/phpcs-fix.sh
+++ bin/phpcs-fix.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(cd `dirname $0` && pwd)
 PLUGIN_DIR=$(dirname "$BASEDIR")
-SHOPWARE_DIR=$(dirname "$PLUGIN_DIR")/../../
+SHOPWARE_DIR=$(dirname "$PLUGIN_DIR")/../project
 
 "$SHOPWARE_DIR"/vendor/bin/php-cs-fixer fix --using-cache=no --verbose --config="$PLUGIN_DIR"/.php_cs "$PLUGIN_DIR";
 
