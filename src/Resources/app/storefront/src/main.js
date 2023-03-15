8066527cace1cefafd8dfd49ca8f87bff504e201
Frederik Rommel <frederik.rommel@webidea24.de>
2023-03-15 16:17:01 +0100

    RATESWSX-275: sw65-compatibility: storefront: improve js-plugin register

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,29 +1,20 @@
 /*
- * Copyright (c) 2020 Ratepay GmbH
+ * Copyright (c) Ratepay GmbH
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-// Import all necessary Storefront plugins and scss files
 import RatepayInstallment from './RatepayCheckout/Installment';
 import RatepayInstallmentPaymentSwitch from './RatepayCheckout/InstallmentPaymentSwitch';
 
-// Register them via the existing PluginManager
 const PluginManager = window.PluginManager;
 let pluginList = PluginManager.getPluginList();
 
-if(typeof pluginList.RatepayInstallment === undefined || typeof pluginList.RatepayInstallment === 'undefined') {
-    PluginManager.register('RatepayInstallment',
-        RatepayInstallment,
-        '[data-ratepay-installment="true"]'
-    );
+if(!('RatepayInstallment' in pluginList)) {
+    PluginManager.register('RatepayInstallment', RatepayInstallment, '[data-ratepay-installment="true"]');
 }
 
-if(typeof pluginList.RatepayInstallmentPaymentSwitch === undefined || typeof pluginList.RatepayInstallmentPaymentSwitch === 'undefined') {
-    PluginManager.register(
-        'RatepayInstallmentPaymentSwitch',
-        RatepayInstallmentPaymentSwitch,
-        '[data-ratepay-installment-payment-switch="true"]'
-    );
+if(!('RatepayInstallmentPaymentSwitch' in pluginList)) {
+    PluginManager.register('RatepayInstallmentPaymentSwitch', RatepayInstallmentPaymentSwitch, '[data-ratepay-installment-payment-switch="true"]');
 }
