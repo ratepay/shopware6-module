2c53baebcae0db45360796f2ceecaffb7522d181
Frederik Rommel <frederik.rommel@webidea24.de>
2023-09-13 17:29:43 +0200

    ecs: remove legacy method-call (SW 6.4.x)

--- ecs.php
+++ ecs.php
@@ -7,7 +7,6 @@ use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSpaceFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
@@ -34,8 +33,8 @@ file that was distributed with this source code.', 'separate' => 'bottom', 'loca
         AssignmentInConditionSniff::class
     ]);
 
-    $parameters = $ecsConfig->parameters();
-
-    $parameters->set(Option::CACHE_DIRECTORY, __DIR__ . '/var/cache/cs_fixer');
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests'
+    ]);
 };
