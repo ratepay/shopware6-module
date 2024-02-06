e0b27e69ffb490c57c88ceb6504056783554a745
Frederik Rommel <frederik.rommel@webidea24.de>
2024-02-06 14:42:39 +0100

    fix code style

--- ecs.php
+++ ecs.php
@@ -7,36 +7,30 @@ use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSpaceFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-
-    $ecsConfig->paths([
+return ECSConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests'
-    ]);
-
-    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, ['header' => 'Copyright (c) Ratepay GmbH
-
-For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.', 'separate' => 'bottom', 'location' => 'after_declare_strict', 'comment_type' => 'comment']);
-
-    $ecsConfig->sets([
+    ])
+    ->withSets([
         SetList::CLEAN_CODE,
         SetList::COMMON,
         SetList::STRICT,
         SetList::PSR_12,
-    ]);
-
-    $ecsConfig->rules([
-        PsrAutoloadingFixer::class
-    ]);
-
-    $ecsConfig->skip([
+    ])
+    ->withRules([
+        PsrAutoloadingFixer::class,
+    ])
+    ->withSkip([
         ProtectedToPrivateFixer::class,
         NotOperatorWithSpaceFixer::class,
         NotOperatorWithSuccessorSpaceFixer::class,
         AssignmentInConditionSniff::class
-    ]);
-};
+    ])
+    ->withConfiguredRule(HeaderCommentFixer::class, ['header' => 'Copyright (c) Ratepay GmbH
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.', 'separate' => 'bottom', 'location' => 'after_declare_strict', 'comment_type' => 'comment']);
+
